9745f470ce69bb8e15abc8bdad2effefbb8901e1
Irfan <irfanullahkhattak744@gmail.com>
2024-04-30 22:48:15 -0400

    Done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,70 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyExercises();
+
+            int added = Addition(2, 2);
+            Console.WriteLine(added);
+
+            int subtracted = Subtraction(5, 2);
+            Console.WriteLine(subtracted);
+
+            int multiplied = Multiplication(4, 2);
+            Console.WriteLine(multiplied);
+
+            int divided = Division(10, 2);
+            Console.WriteLine(divided);
         }
+
+
+        static void MyExercises()
+        {
+            // Exercise One
+            Console.WriteLine("Hello, what is your name?: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?: ");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Animal?: ");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?: ");
+            string band = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine($"name = {name}");
+            Console.WriteLine($"color = {color}");
+            Console.WriteLine($"animal = {animal}");
+            Console.WriteLine($"band = {band}");
+
+            Console.WriteLine("");
+            Console.WriteLine($"{name} A lover of {color} color, kept a {animal}. They jammed to the {band} every weekend.");
+
+        }
+
+        public static int Addition(int num1, int num2)
+        {
+             
+            return num1 + num2;
+        }
+
+        public static int Subtraction(int num1, int num2)
+        {
+
+            return num1 - num2;
+        }
+
+        public static int Multiplication(int num1, int num2)
+        {
+
+            return num1 * num2;
+        }
+        public static int Division(int num1, int num2)
+        {
+
+            return num1 / num2;
+        }
+
     }
 }
